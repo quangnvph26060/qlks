c2f007819abd9e26187f145354a65258b5064ee9
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-10-04 10:40:28 +0700

    update

--- core/resources/views/templates/basic/layouts/app.blade.php
+++ core/resources/views/templates/basic/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title> {{ gs()->siteName(__($pageTitle)) }}</title>
     @include('partials.seo')
