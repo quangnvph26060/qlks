c2c1ba8978a1e349b5f67023bd2967a01510a043
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-11-05 11:03:22 +0700

    update code

--- core/app/Listeners/SendRoomCancellationEmail.php
+++ core/app/Listeners/SendRoomCancellationEmail.php
@@ -28,10 +28,6 @@ class SendRoomCancellationEmail
         $canceledRooms = $event->data['bookingRequest']->bookingItems->whereIn('room_id', $event->data['cancelledRooms']);
         $data =  findTemplateEmail($event->data['type']);
         $subject    = $data->subject;
-        // Mail::send('templates.mail.email-register', ['fullname'=>$fullname, 'username'=>$username, 'password'=>$password], 
-        // function($message) use ($username,$subject) {
-        //     $message->to($username)->subject($subject);
-        // });
         try {
             Mail::to($user->email)->send(new RoomCancellationMail($canceledRooms, $user, $event->data['bookingRequest']));
             Log::info('Email queued successfully for user: ' . $user->email);
