e0d6bc4f9c34c87d0bba0e3656be73241d1521df
devquangnv <118239221+quangnvph26060@users.noreply.github.com>
2024-11-11 08:20:23 +0700

    Revert "update management price room"

--- core/app/Models/RoomPrice.php
+++ core/app/Models/RoomPrice.php
@@ -8,52 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class RoomPrice extends Model
 {
     use HasFactory;
-    protected $table  = 'rooms_prices';
 
-    // const STATUS_ACTIVE = 'active';
-    // const STATUS_INACTIVE = 'inactive';
+    const STATUS_ACTIVE = 'active';
+    const STATUS_INACTIVE = 'inactive';
 
-    // protected $fillable = [
-    //     'code',
-    //     'name',
-    //     'price',
-    //     'start_date',
-    //     'end_date',
-    //     'status',
-    //     'start_time',
-    //     'end_time',
-    //     'specific_date'
-    // ];
-
-    // public function scopeActive($query)
-    // {
-    //     return $query->where('status', self::STATUS_ACTIVE);
-    // }
     protected $fillable = [
-        'room_id',
-        'price_type_id',
-        'day_of_week_id',
-        'first_hour',
-        'additional_hour',
-        'full_day',
-        'overnight',
+        'code',
+        'name',
+        'price',
+        'start_date',
+        'end_date',
+        'status',
+        'start_time',
+        'end_time',
+        'specific_date'
     ];
 
-    // public function rooms()
-    // {
-    //     return $this->belongsToMany(Room::class, 'room_price_rooms', 'price_id', 'room_id')->withPivot('start_date', 'end_date', 'start_time', 'end_time', 'specific_date', 'status');
-    // }
+    public function scopeActive($query)
+    {
+        return $query->where('status', self::STATUS_ACTIVE);
+    }
 
     public function rooms()
     {
-        return $this->belongsTo(Room::class, 'room_id');
-    }
-    public function priceType()
-    {
-        return $this->belongsTo(PriceType::class, 'price_type_id');
-    }
-    public function dayOfWeek()
-    {
-        return $this->belongsTo(DayOfWeek::class, 'day_of_week_id');
+        return $this->belongsToMany(Room::class, 'room_price_rooms', 'price_id', 'room_id')->withPivot('start_date', 'end_date', 'start_time', 'end_time', 'specific_date', 'status');
     }
 }
