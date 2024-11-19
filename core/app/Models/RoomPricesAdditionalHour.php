d53b1259a48f0289ac24892eef48b33732b9080e
thangngo.dev <thangngo130802@gmail.com>
2024-11-19 09:12:31 +0700

    update_price_day

---
+++ core/app/Models/RoomPricesAdditionalHour.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RoomPricesAdditionalHour extends Model
+{
+    protected $table = 'room_prices_additional_hour';
+    protected $guarded = [];
+    public $timestamps = true;
+}
