9057796375be3f2b871be43944cd33aa3e4ae2a1
quangnvph26060 <sgovietnam.dev@gmail.com>
2025-08-18 17:06:25 +0700

    up css

--- core/resources/views/admin/booking/partials/room_booking_edit.blade.php
+++ core/resources/views/admin/booking/partials/room_booking_edit.blade.php
@@ -39,7 +39,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body "
-                style="padding: 5px 12px 12px 5px; overflow-x: auto; scrollbar-width: none; -ms-overflow-style: none;">
+                style="padding: 5px 12px 12px 5px; overflow-x: auto; scrollbar-width: none; -ms-overflow-style: none; margin-bottom: 20px;">
 
 
                 <form id="bookingForm" action="{{ route('admin.room.book.edit') }}" class="booking-form-edit"
